d9ac18fa862b4eb69887a35bda4e0153da7304e5
Arabhya07092007 <r.arabhaya@gmail.com>
2021-07-24 14:41:13 +0530

    text ko align kiya center me

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
 function setup() {
-createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(255);
   let display = touches.length + " touches";
-  text(display, 5, 10);
+  text(display, windowWidth / 2, windowHeight / 2);
 }
+
+// echo "# React-native-game" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Arabhya07092007/React-native-game.git
+// git push -u origin main
